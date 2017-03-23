58e174b49071cef855491776563dd0ff784f7629
高子健 <gaozijian0411@outlook.com>
2017-03-23 13:18:15 +0800

    动画和颜色调整，以及尝试添加列表页

--- clf_blog/wwwroot/js/main.js
+++ clf_blog/wwwroot/js/main.js
@@ -14,5 +14,24 @@ $(function () {
         {
             eles.addClass("active");
         }
-    })
+    });
 });
+//这里声明公用函数 使用System命名空间
+var System={};
+System.LoadPage=function (name,query) {
+    //此函数根据name加载一个页面到maincontent中
+    //query为查询参数
+    var href="/Home/"+name;
+    $("#maincontent").load(href,query);
+}
+System.LoadSide=function (name,query) {
+    //由指定的查询参数从指定位置加载页面到main-sidecont中
+    var href="/Sides/"+name;
+    $("#main-sidecont").load(name,query);
+}
+System.ShowArticle=function(id)
+{
+    //显示文章并且 显示侧边栏
+    System.LoadPage('Article',{Id:id});
+    System.LoadSide('Article',{Id:id});
+}
