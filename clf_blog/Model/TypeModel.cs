8c0cf2bef2a1f0164a4e39ff03577404081f4d65
高子健 <gaozijian0411@outlook.com>
2017-03-30 19:42:53 +0800

    添加TypeModel模型类

---
+++ clf_blog/Model/TypeModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.IO;
+namespace clf_blog.Model
+{
+    /// <summary>
+    /// 此类提供对类型列表和描述信息的访问
+    /// </summary>
+    public class TypeModel
+    {
+        public class TypeInfo
+        {
+            public long Id;
+            public string Name;
+            public string Info;
+        }
+        static string source = null;
+        public TypeModel()
+        {
+            if (source == null)
+            {
+                var builder = new SqliteConnectionStringBuilder();
+                DirectoryInfo info = new DirectoryInfo("Data/MainDB.sqlite3");
+                builder.DataSource = info.FullName;
+                source = builder.ConnectionString;
+            }
+        }
+        public TypeInfo ToType(SqliteDataReader reader)
+        {
+            bool isok = reader.Read();
+            if (isok)
+            {
+                TypeInfo ret = new TypeInfo();
+                ret.Id = reader.GetInt64(0);
+                ret.Name = reader.GetString(1);
+                ret.Info = reader.GetString(2);
+                return ret;
+            }
+            return null;
+        }
+    }
+
+}
